883f7b8b459c55284ad342cf612048243f2a5e2e
matvey <matveyvs1987@gmail.com>
2023-09-06 16:02:35 +0200

    Create Java Technology jsp file "Обзор java enterprise технологий" for others feature is not ready message.

--- src/main/java/by/javaguru/git/mergeexperience/Module1Servlet.java
+++ src/main/java/by/javaguru/git/mergeexperience/Module1Servlet.java
@@ -24,12 +24,21 @@ public class Module1Servlet extends HttpServlet {
 
         out.println("<table>");
         for (Module1Topics value : Module1Topics.values())
-            out.println("<tr><td>"
-                        + value.getOrder() + "</td><td>"
-                        + value.getTopic() + "</td><td>"
-                        + value.getDesc()
-                        + "</td><td><a href=''>Подробнее</a>"
-                        + "</td></tr>");
+            if (value.getOrder() == 1) {
+                out.println("<tr><td>"
+                            + value.getOrder() + "</td><td>"
+                            + value.getTopic() + "</td><td>"
+                            + value.getDesc()
+                            + "</td><td><a href='javaTechnologies.jsp'>Подробнее</a>"
+                            + "</td></tr>");
+            } else {
+                out.println("<tr><td>"
+                            + value.getOrder() + "</td><td>"
+                            + value.getTopic() + "</td><td>"
+                            + value.getDesc()
+                            + "</td><td><a href='notReady.jsp'>Подробнее</a>"
+                            + "</td></tr>");
+            }
 
         out.println("</table>");
 
---
+++ src/main/webapp/notReady.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Feature is not ready</title>
+</head>
+<body>
+<h1>Feature is not ready</h1>
+</body>
+</html>
